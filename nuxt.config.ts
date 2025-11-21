b7090ebd3d386f6c2c79b81bfd2900114e81147c
Cesare Torchia <cesare@torchia.eu>
2025-11-21 20:44:06 +0100

    removed unused module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/test-utils', '@nuxt/image', '@nuxt/content'],
+  modules: ['@nuxt/ui', '@nuxt/image', '@nuxt/content'],
   css: ['~/assets/css/main.css'],
-  content: {
-    // highlight: {
-    //   theme: 'github-dark'
-    // }
-  },
+  content: {},
   ui: {
     theme: {
       colors: ['primary', 'secondary', 'success', 'info', 'warning', 'error', 'purple', 'green']
